af828c117abfdb8b97f7af6a4a9c5bcd8bae7e58
ASAHI <renyum0918@gmail.com>
2020-12-03 22:31:15 +0800

    2nd submit rev

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -4,10 +4,6 @@ public class OffByN implements CharacterComparator {
         N = x;
     }
     public boolean equalChars(char x, char y) {
-        if (!((((x < 91) && (x > 64)) && ((x < 123) && (x > 96)))
-                && (((y < 91) && (y > 64)) && ((y < 123) && (y > 96))))) {
-            return false;
-        }
         return ((x - y) * (x - y) == N * N);
     }
 }
--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -8,10 +8,18 @@ public class TestOffByOne {
     static CharacterComparator offByOne = new OffByOne();
     @Test
     public void testEqualChars() {
-        assertTrue(offByOne.equalChars('a', 'b'));
+        offByOne.equalChars('a', 'b');
+        assertTrue(offByOne.equalChars('A', 'B'));
         assertFalse(offByOne.equalChars('v', 'v'));
-        assertTrue(offByOne.equalChars('v', 'u'));
-        assertTrue(offByOne.equalChars('u', 'v'));
+        assertFalse(offByOne.equalChars('A', 'A'));
+        assertTrue(offByOne.equalChars('A', 'B'));
+        assertTrue(offByOne.equalChars('B', 'A'));
+        assertFalse(offByOne.equalChars('A', 'C'));
+        assertFalse(offByOne.equalChars('C', 'A'));
+        assertTrue(offByOne.equalChars('%', '&'));
+        assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('$', '&'));
+        assertFalse(offByOne.equalChars('&', '$'));
     }
     // Your tests go here.
     //Uncomment this class once you've created your Character
