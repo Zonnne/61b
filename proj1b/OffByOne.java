87901358ad76470103337b17e85d952b6e0f46aa
ASAHI <renyum0918@gmail.com>
2020-12-03 21:42:33 +0800

    2nd submit

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -1,7 +1,7 @@
 public class OffByOne implements CharacterComparator {
     public boolean equalChars(char x, char y) {
-        if ((((x < 91) && (x > 64)) && ((x < 123) && (x > 96)))
-            && (((y < 91) && (y > 64)) && ((y < 123) && (y > 96)))) {
+        if (!((((x < 91) && (x > 64)) && ((x < 123) && (x > 96)))
+            && (((y < 91) && (y > 64)) && ((y < 123) && (y > 96))))) {
             return false;
         }
         return ((x - y) * (x - y) == 1);
